b2f2dc8991481a6601556767236f93a12080c326
MikeMasonsCreations <64913725+MikeMasonsCreations@users.noreply.github.com>
2022-05-17 13:04:12 -0400

    Fixed bugs with state errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,27 @@
 import { useState } from "react";
-import styles from "./modules/form.module.css";
-import axios from "axios";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ProductForm from "./components/ProductsForm";
+import Product from "./components/Product"
+import ProductList from "./components/ProductList";
 
 function App() {
 
-  const defaultForm = {
-    title : "",
-    price : 0.00,
-    description : ""
-  };
-
-  const [formData, setFormData] = useState(defaultForm);
-
-  function submitHandler(e){
-    e.preventDefault();
-    axios.post("http://localhost:8000/api/products", {...formData})
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-    setFormData(defaultForm);
-  }
-
-  function changeHandler(e){
-    setFormData({...formData,
-      [e.target.name] : e.target.value});
-  }
+  const [submitted, setSubmitted] = useState(false);
 
   return (
-    <div className={styles.App}>
-      <h1 className={styles.formtitle}>Product Manager</h1>
-      <form action="" className={styles.formitem} onSubmit={submitHandler}>
-        <div className={styles.formcontent}>
-          <label htmlFor="title">Title:</label>
-          <input name='title' type="text" placeholder="Insert Title" onChange={changeHandler} value={formData.title}/>
-        </div>
-        <div className={styles.formcontent}>
-          <label htmlFor="price">Price:</label>
-          <input name='price' type="number" step="0.01" min="0.00" placeholder="0" onChange={changeHandler} value={formData.price}/>
-        </div>
-        <div className={styles.formcontent}>
-          <label htmlFor="description">Description:</label>
-          <input name='description' type="text" placeholder="Insert Description" onChange={changeHandler} value={formData.description}/>
-        </div>
-        <button type="submit" className={styles.submitbutton}>Submit</button>
-      </form>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/product/:id">
+            <Product />
+          </Route>
+          <Route path="/">
+            <ProductForm submitted={submitted} setSubmitted={setSubmitted} />
+            <ProductList submitted={submitted} setSubmitted={setSubmitted} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -2,14 +2,23 @@ const Product = require("../models/product.model");
 
 module.exports.findAllProducts = (req, res) => {
     Product.find()
-        .then(allProducts => res.json({products : allProducts}))
-        .catch(err => res.json({msg : "An Error Occured", error : err}));
+        .then(allProducts => res.json({ products: allProducts }))
+        .catch(err => res.json({ msg: "An Error Occured", error: err }));
 }
 
 module.exports.addAProduct = (req, res) => {
     Product.create(req.body)
         .then(newProduct => {
-            res.json({product : newProduct})
+            res.json({ product: newProduct })
         })
-        .catch(err => res.json({msg : "An Error Occured", error : err}));
+        .catch(err => res.json({ msg: "An Error Occured", error: err }));
+}
+
+module.exports.findOneProduct = (req, res) => {
+    Product.findOne({ _id: req.params.id })
+        .then(oneProduct => {
+            res.json({ product: oneProduct })
+            console.log(oneProduct);
+        })
+        .catch(err => res.json({ msg: "An Error Occured", error: err }));
 }
